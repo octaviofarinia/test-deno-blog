046c536736a93a1fb72e43579ee0538f008a4cf5
Octavio Fariña <octaviofarinia@gmail.com>
2023-02-26 21:01:31 -0300

    deno blog template

---
+++ main.tsx
@@ -0,0 +1,27 @@
+/** @jsx h */
+
+import blog, { ga, redirects, h } from "blog";
+
+blog({
+  title: "Octavio Fariña",
+  description: "Este es un blog de prueba.",
+  // header: <header>Your custom header</header>,
+  // section: <section>Your custom section</section>,
+  // footer: <footer>Your custom footer</footer>,
+  avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
+  avatarClass: "rounded-full",
+  author: "Me",
+
+  // middlewares: [
+
+  // If you want to set up Google Analytics, paste your GA key here.
+  // ga("UA-XXXXXXXX-X"),
+
+  // If you want to provide some redirections, you can specify them here,
+  // pathname specified in a key will redirect to pathname in the value.
+  // redirects({
+  //  "/hello_world.html": "/hello_world",
+  // }),
+
+  // ]
+});
---
+++ posts/hello_world.md
@@ -0,0 +1,6 @@
+---
+title: Hello world!
+publish_date: 2023-02-26
+---
+
+This is my first blog post!
